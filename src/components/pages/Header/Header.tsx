fa2c12efd9e0819a1a6fcef84e608fa22dbdc157
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-25 16:54:00 -0300

    Added Header

---
+++ src/components/pages/Header/Header.tsx
@@ -0,0 +1,15 @@
+//type props
+
+import "./Header.css";
+export default function Header({setPage}:any) {
+  return (
+    <div className="Header">
+      <nav className="Header__navbar">
+        <div onClick={() => setPage("login")}>Login</div>
+        <div onClick={() => setPage("register")}>Register</div>
+        <div onClick={() => setPage("notes")}>Notes</div>
+        <div onClick={() => setPage("repositories")}>Repositories</div>
+      </nav>
+    </div>
+  );
+}
