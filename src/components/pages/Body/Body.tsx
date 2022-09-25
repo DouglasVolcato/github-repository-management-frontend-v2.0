09bcb7677a3214f84a76d4971b4e2c924d556497
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-25 16:53:44 -0300

    Added Body

---
+++ src/components/pages/Body/Body.tsx
@@ -0,0 +1,24 @@
+import Repositories from "../Repositories/Repositories";
+import Notes from "../Notes/Notes";
+// import Login from "../Login/Login";
+// import Register from "../Register/Register";
+import "./Body.css";
+
+export default function Body({page}:any) {
+  function showPage() {
+
+    if (page === "login") {
+    //   return <Login />;
+    } else if (page === "register") {
+    //   return <Register />;
+    } else if (page === "notes") {
+      return <Notes />;
+    } else if (page === "repositories") {
+      return <Repositories />;
+    }
+
+    return <div>{page}</div>
+  }
+
+  return <div className="Body">{showPage()}</div>;
+}
