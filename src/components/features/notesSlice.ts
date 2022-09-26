6c10cc5dabb29d7fb630bea0401b4d64ba521531
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-26 01:18:43 -0300

    Added notesSlice

--- src/components/features/notesSlice.ts
+++ src/components/features/notesSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {Api} from "../functions/api.functions"
 
 interface NotesState {
   value: any;
@@ -13,10 +14,26 @@ const notesSlice = createSlice({
   initialState,
   reducers: {
     addNotes: (state, action: PayloadAction<[]>) => {
-      state.value = action.payload;
+      state.value = Api.sortFunction(action.payload);
+    },
+    addNote: (state, action: PayloadAction<{}>) => {
+      const newState = state.value
+      newState.push(action.payload)
+      state.value = Api.sortFunction(newState)
+    },
+    deleteNote: (state, action: PayloadAction<number>) => {
+      const newState = state.value
+      newState.splice(action.payload,1)
+      state.value = newState
+    },
+    editNote: (state, action: PayloadAction<{index:number, body:{}}>) => {
+      const newState = state.value
+      const newNote = Object.assign(state.value[action.payload.index], action.payload.body)
+      newState.splice(action.payload.index,1, newNote)
+      state.value = Api.sortFunction(newState)
     },
   },
 });
 
-export const { addNotes } = notesSlice.actions;
+export const { addNotes, addNote, deleteNote, editNote } = notesSlice.actions;
 export default notesSlice.reducer;
