9f7632020aafcc05ddc1577d1bd24dabf619da6a
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-26 01:19:19 -0300

    Added userReducer

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import repositoriesReducer from "../features/repositoriesSlice";
 import notesReducer from "../features/notesSlice";
+import userReducer from "../features/userSlice"
 
 export const store = configureStore({
   reducer: {
+    user: userReducer,
     repositories: repositoriesReducer,
     notes: notesReducer,
   },
