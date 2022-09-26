4f97f4875b8928f3c099aa3cf26a4447e0e1fb50
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-26 01:17:35 -0300

    Added Notes

---
+++ src/components/pages/Body/Notes/Notes.tsx
@@ -0,0 +1,19 @@
+import Note from "./Note/Note";
+import "./Notes.css";
+import {useSelector} from "react-redux"
+import { RootState } from "../../../store/store";
+
+export default function Notes() {
+
+const notes = useSelector(
+    (state: RootState) => (state.notes.value)
+)
+
+  return (
+    <div className="Notes">
+      {notes.map((data:any, key:any) => (
+        <Note props={{data: data, key: key}} />
+      ))}
+    </div>
+  );
+}
