0a90f2f5dab42abe00ec2c29f62b250062ee421f
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-26 01:18:53 -0300

    Added userSlice

---
+++ src/components/features/userSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+  value: any;
+}
+
+const initialState: UserState = {
+  value: {},
+};
+
+const userSlice = createSlice({
+  name: "repositories",
+  initialState,
+  reducers: {
+    addUser: (state, action: PayloadAction<{}>) => {
+      state.value = Object.assign(state.value,action.payload) ;
+    },
+  },
+});
+
+export const { addUser } = userSlice.actions;
+export default userSlice.reducer;
