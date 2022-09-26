541bd57129bb25993a76373e617268ee4d037733
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-26 01:18:31 -0300

    Added Repository model

---
+++ src/components/pages/Body/Repositories/Repository/Repository.tsx
@@ -0,0 +1,41 @@
+import { Api } from "../../../../functions/api.functions";
+import { useDispatch } from "react-redux";
+import { addNote } from "../../../../features/notesSlice";
+import "./Repository.css";
+
+export default function Repository({ repo }: any) {
+
+  const dispatch = useDispatch()
+
+  async function createNote() {
+    const body = {
+      name: repo.name,
+      link: repo.svn_url,
+      priority: "Low",
+      deadline: new Date().toISOString().slice(0, 10),
+      note: "Empty",
+    }
+    Api.createNote(body)
+    dispatch(addNote(body))
+  }
+
+  return (
+    <section className="Repository">
+      <div className="Repository__id Repository__name">ID: {repo.id}</div>
+      <div className="Repository__name">{repo.name}</div>
+      <div className="Repository__buttons">
+        <a
+          className="Repository__accessButton"
+          href={repo.svn_url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="Repository__accessButton2">ACCESS</button>
+        </a>
+        <button className="Repository__addButton" onClick={() => createNote()}>
+          ADD NOTE
+        </button>
+      </div>
+    </section>
+  );
+}
