1f603848cb1c29e3b6b90702982c45cf27439940
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-26 01:15:37 -0300

    Added Pagination

--- src/components/pages/Body/Body.tsx
+++ src/components/pages/Body/Body.tsx
@@ -1,23 +1,21 @@
-import Repositories from "../Repositories/Repositories";
-import Notes from "../Notes/Notes";
-// import Login from "../Login/Login";
-// import Register from "../Register/Register";
+import Repositories from "./Repositories/Repositories";
+import Notes from "./Notes/Notes";
+import Login from "./Login/Login";
+import Register from "./Login/Register/Register";
 import "./Body.css";
 
 export default function Body({page}:any) {
   function showPage() {
 
     if (page === "login") {
-    //   return <Login />;
+      return <Login />;
     } else if (page === "register") {
-    //   return <Register />;
+      return <Register />;
     } else if (page === "notes") {
       return <Notes />;
     } else if (page === "repositories") {
       return <Repositories />;
     }
-
-    return <div>{page}</div>
   }
 
   return <div className="Body">{showPage()}</div>;
